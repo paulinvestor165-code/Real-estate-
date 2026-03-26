9963cd4527f2d365a2aecff885acf37f2bb8bfb6
paulinvestor165-code <paulinvestor165@gmail.com>
2026-03-26 06:33:13 +0100

    Implement GET route for fetching properties

---
+++ app/api/property/all/route.js
@@ -0,0 +1,12 @@
+// app/api/property/all/route.js
+
+import { connectDB } from "@/lib/db";
+import Property from "@/models/Property";
+
+export async function GET() {
+  await connectDB();
+
+  const properties = await Property.find().populate("agent");
+
+  return Response.json(properties);
+}
